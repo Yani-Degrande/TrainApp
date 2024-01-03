bc4c738fa847006c9439dbab513c1abdf27a62ae
Yani <yani.degrande@student.hogent.be>
2024-01-03 21:19:09 +0100

    Network tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,11 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
---
+++ app/src/test/java/com/example/trainapp/network/TrainComponentApiServiceTest.kt
@@ -0,0 +1,74 @@
+package com.example.trainapp.network
+
+import com.example.trainapp.model.TrainComponentType
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TrainComponentApiServiceTest {
+    private lateinit var service: TrainComponentApiService
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer()
+        service =
+            Retrofit.Builder()
+                .baseUrl(mockWebServer.url("/"))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(TrainComponentApiService::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `getTrainComponents returns list successfully`() =
+        runBlocking {
+            val sampleApiResponse = "[{\"id\": 1, \"type\": \"LOCOMOTIVE\", \"description\": \"Sample Description\"}]"
+            mockWebServer.enqueue(MockResponse().setBody(sampleApiResponse))
+
+            val response = service.getTrainComponents()
+
+            assertNotNull(response)
+            assertEquals(1, response.size)
+            assertEquals(TrainComponentType.LOCOMOTIVE, response[0].type)
+        }
+
+    @Test
+    fun `getTrainComponentById returns item successfully`() =
+        runBlocking {
+            val sampleApiResponse = "{\"id\": 1, \"type\": \"LOCOMOTIVE\", \"description\": \"Sample Description\"}"
+            mockWebServer.enqueue(MockResponse().setBody(sampleApiResponse))
+
+            val response = service.getTrainComponentById(1)
+
+            assertNotNull(response)
+            assertEquals(1, response.id)
+            assertEquals(TrainComponentType.LOCOMOTIVE, response.type)
+        }
+
+    @Test
+    fun `getTrainComponentsAsFlow emits items successfully`() =
+        runBlocking {
+            val sampleApiResponse = "[{\"id\": 1, \"type\": \"LOCOMOTIVE\", \"description\": \"Sample Description\"}]"
+            mockWebServer.enqueue(MockResponse().setBody(sampleApiResponse))
+
+            val flow = service.getTrainComponentsAsFlow()
+            val resultList = flow.toList()
+
+            assertEquals(1, resultList.size)
+            assertEquals(TrainComponentType.LOCOMOTIVE, resultList[0][0].type)
+        }
+}
